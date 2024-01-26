2d67f9ddaeceed81dd0a7537606e53b8e3f12857
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-01-26 15:08:54 -0300

    feat: adding TMPro and Menu

---
+++ Assets/Scripts/MenuBt.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBt : MonoBehaviour
+{
+    [SerializeField] private Button playBt;
+    [SerializeField] private Button exitBt;
+    [SerializeField] private Button creditsBt;
+
+    [SerializeField] private Button backMenuBt;
+    [SerializeField] private GameObject creditsPanel;
+
+    void Start()
+    {
+        playBt.gameObject.SetActive(true);
+        exitBt.gameObject.SetActive(true);
+        creditsBt.gameObject.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+    public void Play(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Exit(){
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    public void Credits(){
+        if(creditsPanel.activeSelf){
+            creditsPanel.SetActive(false);
+            playBt.gameObject.SetActive(true);
+            exitBt.gameObject.SetActive(true);
+            creditsBt.gameObject.SetActive(true);
+
+        } else{
+            creditsPanel.SetActive(true);
+            playBt.gameObject.SetActive(false);
+            exitBt.gameObject.SetActive(false);
+            creditsBt.gameObject.SetActive(false);
+        }
+
+    }
+}
